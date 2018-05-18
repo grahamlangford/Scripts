bdceb6399406977f8b29052768138dff4364afc1
Graham Langford <grahamlangford87@gmail.com>
2018-05-18 07:38:06 -0500

    added gnome tweak tool to initial script

--- initial.sh
+++ initial.sh
@@ -1,9 +1,14 @@
 echo updating apt
 sudo apt update
 
+echo updateing apt-get
+sudo apt-get update
+
 echo installing vim
 sudo apt install -y vim
 
 echo installing curl
 sudo apt install -y curl
 
+echo installing gnome-tweak-tool
+sudo apt-get install -y gnome-tweak-tool
