b15d9f7a10e846a27713734a9149ff9cb3c58fed
Graham Langford <grahamlangford87@gmail.com>
2018-05-18 10:31:03 -0500

    added devTools script

---
+++ getDevTools.sh
@@ -0,0 +1,8 @@
+echo install node
+sudo apt-get install -y nodejs
+
+echo install yarn 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install -y yarn
--- getVsCode.sh
+++ getVsCode.sh
@@ -4,5 +4,14 @@ curl -o code.deb -L http://go.microsoft.com/fwlink/?LinkID=760868
 echo installing vscode
 sudo dpkg -i code.deb
 
+echo installing extensions
+VSC_EXT_DIR=~/.vscode/extensions
+mkdir -p $VSC_EXT_DIR
+code --user-data-dir=$VSC_CONF_DIR \
+    --extensions-dir=$VSC_EXT_DIR \
+    --install-extension=dbaeumer.vscode-eslint \
+    --install-extension=esbenp.prettier-vscode \
+    --install-extension=jakob101.relativepath
+
 echo cleaning up
 rm code.deb
