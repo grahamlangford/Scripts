ce274105010973fdebeef302ef8f54ba2bff091b
Graham Langford <grahamlangford87@gmail.com>
2018-10-06 11:03:56 -0500

    added curl command for pulling node v8 deb

--- getDevTools.sh
+++ getDevTools.sh
@@ -1,5 +1,6 @@
 echo install node
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt install -y nodejs
 
 echo install yarn 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
